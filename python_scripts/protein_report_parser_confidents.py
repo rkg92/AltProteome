e68331f5e0edf802385051fbf58038af1a800f2a
Drew Vander Wood <dvanderwood@gmail.com>
2017-04-27 10:11:08 -0500

    unique_confident_proteins_update

--- python_scripts/protein_report_parser_confidents.py
+++ python_scripts/protein_report_parser_confidents.py
@@ -1,4 +1,5 @@
-#This script takes in a directory containing protein reports from peptide shaker and determine number of confident proteins identified.
+#This script takes in a directory containing protein reports from peptide shaker and determine number of confident, unique proteins identified.
+#It outputs a file that shows the protein by accession and the number of times it was seen confidently.
 
 '''
 Usage: protein_report_parser_confident.py protein_report_file_directory
@@ -11,6 +12,7 @@ folder = sys.argv[1]
 reports = glob.glob(folder + '*Protein_Report.txt')
 
 confident_proteins = 0
+proteins_set = {}
 
 for report in reports:
 	with open(report,'r') as report_file:
@@ -18,6 +20,17 @@ for report in reports:
 		for line in report_file:
 			line_split = line.strip().split('\t') #split tab delim line
 			if line_split[-1] == 'Confident': #check if protein is confident
-				confident_proteins += 1
+				if not line_split[1] in proteins_set:
+					proteins_set[line_split[1]] = 1
+					confident_proteins += 1
+				else:
+					proteins_set[line_split[1]] += 1
+
+
 
 print('%i Confident Proteins identified' % confident_proteins)
+
+with open(folder[:-1] + '_unique_confident_proteins.txt', 'w') as out_file:
+	out_file.write('protein\tcount\n')
+	for key in proteins_set:
+		out_file.write('%s\t%i\n' % (key, proteins_set[key]))
