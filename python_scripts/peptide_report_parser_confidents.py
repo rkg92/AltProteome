f75953f2d98e52ca7af9780296ec9a466e0f5386
Drew Vander Wood <dvanderwood@gmail.com>
2017-04-27 11:41:07 -0500

    fixed_reports_scrips_and_results

--- python_scripts/peptide_report_parser_confidents.py
+++ python_scripts/peptide_report_parser_confidents.py
@@ -20,17 +20,17 @@ for report in reports:
 		for line in report_file:
 			line_split = line.strip().split('\t') #split tab delim line
 			if line_split[-1] == 'Confident': #check if protein is confident
-				if not line_split[1] in proteins_set:
-					proteins_set[line_split[1]] = 1
+				if not line_split[5] in proteins_set:
+					proteins_set[line_split[5]] = 1
 					confident_proteins += 1
 				else:
-					proteins_set[line_split[1]] += 1
+					proteins_set[line_split[5]] += 1
 
 
 
-print('%i Confident Proteins identified' % confident_proteins)
+print('%i Confident Sequences identified' % confident_proteins)
 
 with open(folder[:-1] + '_unique_confident_peptide.txt', 'w') as out_file:
-	out_file.write('protein\tcount\n')
+	out_file.write('sequence\tcount\n')
 	for key in proteins_set:
 		out_file.write('%s\t%i\n' % (key, proteins_set[key]))
